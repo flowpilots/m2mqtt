791a6197126dfc11625f46145f8105c1b01becb8
ppatierno_cp <SND\ppatierno_cp>
2014-04-03 23:43:36 -0700

    1. MqttSettings : removed unused namespaces;

--- M2Mqtt/MqttSettings.cs
+++ M2Mqtt/MqttSettings.cs
@@ -16,9 +16,6 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library.
 */
 
-using System;
-using System.Text;
-
 namespace uPLibrary.Networking.M2Mqtt
 {
     /// <summary>
